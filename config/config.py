5fa9c05218f67bce88fcad9838087d51443d2b8a
Ishu_official <somnathhor12052005@gmail.com>
2024-09-21 11:18:43 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -109,11 +109,11 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/KDramaDGGhindidub"
-)  # Example:- https://t.me/TheTeamVivek
+    "SUPPORT_CHANNEL", "https://t.me/somueditingzone"
+)  # Example:- https://t.me/somueditingzone 
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/music_bot_support1"
-)  # Example:- https://t.me/TheTeamVk
+    "SUPPORT_GROUP", "https://t.me/+7kCwP6b4qEkxZjZl"
+)  # Example:- https://t.me/somueditingzone 
 
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
